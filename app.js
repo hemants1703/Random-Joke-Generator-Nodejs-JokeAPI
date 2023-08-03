083b8d2a86f064bdb21688c5d7ea9b0420d4ce1c
Hemant Sharma <58618028+hemants1703@users.noreply.github.com>
2023-08-03 21:17:23 +0530

    Update app.js
    added ```process.env.PORT``` to PORT value for deployment purposes

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const https = require("https");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
